20ddb786c0bf85abe4ad72b648cb6b4239e0bcce
jrpbjr <jrpbjr@gmail.com>
2024-09-19 22:04:47 -0300

    implementando metodo para abertura da  modal

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { TodoFormComponent } from './../todo-form/todo-form.component';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-header',
@@ -19,7 +20,13 @@ import { MatDialogModule } from '@angular/material/dialog';
   styleUrls: [],
 })
 export class HeaderComponent {
+  private dialogService = inject(MatDialog);
+
   public handleOpenModal(): void {
-    alert('Adicionar nova tarefa');
+    //alert('Adicionar nova tarefa');
+    this.dialogService.open(TodoFormComponent,{
+      width: '50vw',
+      maxHeight: '80vh',
+    });
   }
 }
